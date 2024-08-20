ea9b85351e75c3ef132da6fab21bbffb35fe3c74
PHRAX8 <lootphrax8@gmail.com>
2024-08-20 17:11:50 -0300

    uncomment app.listen

--- backend/api/index.js
+++ backend/api/index.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
 connectToDatabase();
 
 // Start server
-/*const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is Running on Port ${PORT}`);
-});*/
+});
